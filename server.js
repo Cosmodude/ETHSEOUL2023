0c5db1e9689c1c2851fb2fe245b7d9b494081468
Hackdady <lenskii.vm19@physics.msu.ru>
2023-06-04 12:29:49 +0900

    server finished

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const app = express();
-const port = 3000 || process.env.PORT;
 const Web3 = require('web3');
-const truffle_connect = require('./connection/app.js');
+import cors from 'cors';
 const bodyParser = require('body-parser');
+const { tokenCounter, mintToken } = require('./SmartContractMethods.mjs')
 
+const app = express();
+app.use(cors());
+const port = 8080;
+const my_address = "0x6f9e2777D267FAe69b0C5A24a402D14DA1fBcaA1";
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -13,47 +16,21 @@ app.use(bodyParser.json());
 
 app.use('/', express.static('public_static'));
 
-app.get('api/getAccounts', (req, res) => {
-  console.log("**** GET /getAccounts ****");
-  truffle_connect.start(function (answer) {
-    res.send(answer);
-  })
+app.get('api/tokenCounter', async (req, res) => {
+  console.log("**** GET /tokenCounter ****");
+  const counter = await tokenCounter();
+  res.status(200).set('Content-Type', 'application/json').send(counter);
 });
 
-app.post('api/getBalance', (req, res) => {
-  console.log("**** GET /getBalance ****");
-  console.log(req.body);
-  let currentAcount = req.body.account;
-
-  truffle_connect.refreshBalance(currentAcount, (answer) => {
-    let account_balance = answer;
-    truffle_connect.start(function(answer){
-      // get list of all accounts and send it along with the response
-      let all_accounts = answer;
-      response = [account_balance, all_accounts]
-      res.send(response);
-    });
-  });
-});
-
-app.post('api/sendCoin', (req, res) => {
-  console.log("**** GET /sendCoin ****");
-  console.log(req.body);
-
-  let amount = req.body.amount;
-  let sender = req.body.sender;
-  let receiver = req.body.receiver;
-
-  truffle_connect.sendCoin(amount, sender, receiver, (balance) => {
-    res.send(balance);
-  });
+app.post("/api/claimPOM", async (req, res) => {
+  const userAddress = req.body.address ||  ;
+  console.log('**** Claiming POM... ****');
+  const minted = await mintToken(userAddress, 0);
+  res.status(200).set('Content-Type', 'application/json').send(minted.hash);
 });
 
 app.listen(port, () => {
-
-  // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-  truffle_connect.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
-
-  console.log("Express Listening at http://localhost:" + port);
-
+    console.log('server running on port 8080');
 });
+
+
